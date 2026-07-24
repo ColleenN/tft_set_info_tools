67941878c3ecb9e42b96fd55a2706aa4c8c02bde
CNelson <colleenfnelson@gmail.com>
2026-07-24 17:32:44 -0600

    Initial commit

---
+++ src/tft_set_info_tools/__init__.py
@@ -0,0 +1,16 @@
+from tft_set_info_tools.datasource import (
+    CDragonDataSource,
+    GCPDataSource,
+    LocalDataSource,
+    TFTDataSource,
+)
+
+__version__ = "0.1.0"
+
+__all__ = [
+    "__version__",
+    "TFTDataSource",
+    "LocalDataSource",
+    "GCPDataSource",
+    "CDragonDataSource",
+]
---
+++ tests/test_version.py
@@ -0,0 +1,5 @@
+from tft_set_info_tools import __version__
+
+
+def test_version_is_set():
+    assert __version__
